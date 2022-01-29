21e75d2674a4f973123c87592c9374fe8a1717ff
Gilmar Santana Lins <gilmar.livecont@gmail.com>
2022-01-29 18:41:45 -0300

    Tipos de dados

---
+++ Fundamentos_do_JavaScript_Tipos_variáveis_e_funções/typeBoolean.js
@@ -0,0 +1,15 @@
+const primeiroNumero = 5;
+const segundoNumero = 10;
+
+// compara se igual
+console.log(primeiroNumero === segundoNumero);
+
+
+// compara se maior/menor
+console.log(1 > 2);
+console.log(2 < 3)
+
+
+// compara se maior ou igual/menor ou igual por
+console.log(4 >= 5);
+console.log(3 <= 3)
---
+++ Fundamentos_do_JavaScript_Tipos_variáveis_e_funções/typeNumber.js
@@ -0,0 +1,19 @@
+// Tipo de dado Nunber
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacaoMatematicaSoma = primeiroNumero + segundoNumero;
+
+const operacaoMatematicaSubtracao = primeiroNumero - segundoNumero;
+
+const operacaoMatematicaMultiplicacao = primeiroNumero * segundoNumero;
+
+const operacaoMatematicaDivisao = primeiroNumero / segundoNumero;
+
+console.log(operacaoMatematicaDivisao)
+
+// NaN - Not a Number
+console.log(1 * "dois");
