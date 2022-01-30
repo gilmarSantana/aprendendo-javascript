39d48345a59ff65ad532b60d03d6f0d26d8e52a6
Gilmar Santana Lins <65671201+gilmarSantana@users.noreply.github.com>
2022-01-29 23:34:04 -0300

    Add files via upload

--- Fundamentos_do_JavaScript_Tipos_variáveis_e_funções/typeBoolean.js
+++ Fundamentos_do_JavaScript_Tipos_variáveis_e_funções/typeBoolean.js
@@ -13,3 +13,12 @@ console.log(2 < 3)
 // compara se maior ou igual/menor ou igual por
 console.log(4 >= 5);
 console.log(3 <= 3)
+
+
+// case sensitive
+const minhaVar = 1;
+const MinhaVar = "texto";
+const minhavar = "3";
+const MINHAVAR = 2;
+
+console.log(minhaVar, MinhaVar, minhavar, MINHAVAR)
