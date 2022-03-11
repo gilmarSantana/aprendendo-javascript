18570dbc9905da9c14200850f4b2e9058564a9c0
Gilmar Santana Lins <gilmar.santana@nybc.com.br>
2022-03-11 14:24:57 -0300

    aula 13 - Laços de repetição (while, do while)

---
+++ CursoEmVideo/cursoJS/aula13/ambiente.js
@@ -0,0 +1,15 @@
+contador = 1;
+while(contador <= 100) {
+    console.log('Runnig on port 808:' + contador);
+    contador++;
+}
+
+
+
+
+counter = 1;
+do {
+    console.log('Verificando ' + counter);
+    counter++
+} while (counter > 20)
+
