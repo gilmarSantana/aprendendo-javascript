811e1d8f48c8df796e7ac3ed97b9daded6d8c2a7
Gilmar Santana Lins <gilmar.santana@nybc.com.br>
2022-03-11 14:45:20 -0300

    Aula 14 laço de repetição FOR

---
+++ CursoEmVideo/cursoJS/aula14/ambiente.js
@@ -0,0 +1,5 @@
+const code =  ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T'];
+
+for (let index = 0; index < code.length; index++) {
+    console.log(`The internal code is: ${code[index]}`);
+}
